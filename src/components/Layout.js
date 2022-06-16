d146e6d175f9bec9f01dac1236368754b71513d9
uuganbayar dashdavaa <uuganbayar@erdenetmc.mn>
2022-06-16 23:46:40 +0800

    asd

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import FooterMenu from "./FooterMenu";
+import Footer from "./Footer";
 
 const Layout = ({ children }) => {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <Header />
+      <div className="mt-[58px]">{children}</div>
+      <Sidebar />
+      <FooterMenu />
+      <Footer />
+    </div>
+  );
 };
 
 export default React.memo(Layout);
