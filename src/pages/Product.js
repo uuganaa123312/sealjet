c9e5d7ebeb1825bf9763a115fe8c327898d8b88c
uuganbayar dashdavaa <uuganbayar@erdenetmc.mn>
2022-06-22 23:01:05 +0800

    asd

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -3,84 +3,44 @@ import React from "react";
 const Product = () => {
   return (
     <div className="pt-[116px] flex justify-center">
-      <div className="grid grid-cols-2 gap-4 p-4 sm:hidden">
-        <div>
+      <div className="grid grid-cols-2 sm:hidden">
+        <div className="p-4">
           <img
             src="https://utmunbox.com/wp-content/uploads/2020/03/500x500.png"
             alt=""
-            className="w-[170px] h-[170px] object-cover bg-gray-500 rounded-lg"
+            className="object-cover bg-gray-500 rounded-lg"
           />
           <div>Сальник 212.232</div>
         </div>
-        <div>
+        <div className="p-4">
           <img
             src="https://utmunbox.com/wp-content/uploads/2020/03/500x500.png"
             alt=""
-            className="w-[170px] h-[170px] object-cover bg-gray-500 rounded-lg"
+            className="object-cover bg-gray-500 rounded-lg"
           />
           <div>Сальник 212.232</div>
         </div>
-        <div>
+        <div className="p-4">
           <img
             src="https://utmunbox.com/wp-content/uploads/2020/03/500x500.png"
             alt=""
-            className="w-[170px] h-[170px] object-cover bg-gray-500 rounded-lg"
+            className="object-cover bg-gray-500 rounded-lg"
           />
           <div>Сальник 212.232</div>
         </div>
-        <div>
+        <div className="p-4">
           <img
             src="https://utmunbox.com/wp-content/uploads/2020/03/500x500.png"
             alt=""
-            className="w-[170px] h-[170px] object-cover bg-gray-500 rounded-lg"
+            className="object-cover bg-gray-500 rounded-lg"
           />
           <div>Сальник 212.232</div>
         </div>
-        <div>
+        <div className="p-4">
           <img
             src="https://utmunbox.com/wp-content/uploads/2020/03/500x500.png"
             alt=""
-            className="w-[170px] h-[170px] object-cover bg-gray-500 rounded-lg"
-          />
-          <div>Сальник 212.232</div>
-        </div>
-        <div>
-          <img
-            src="https://utmunbox.com/wp-content/uploads/2020/03/500x500.png"
-            alt=""
-            className="w-[170px] h-[170px] object-cover bg-gray-500 rounded-lg"
-          />
-          <div>Сальник 212.232</div>
-        </div>
-        <div>
-          <img
-            src="https://utmunbox.com/wp-content/uploads/2020/03/500x500.png"
-            alt=""
-            className="w-[170px] h-[170px] object-cover bg-gray-500 rounded-lg"
-          />
-          <div>Сальник 212.232</div>
-        </div>
-        <div>
-          <img
-            src="https://utmunbox.com/wp-content/uploads/2020/03/500x500.png"
-            alt=""
-            className="w-[170px] h-[170px] object-cover bg-gray-500 rounded-lg"
-          />
-          <div>Сальник 212.232</div>
-        </div>
-        <div>
-          <img
-            src="https://utmunbox.com/wp-content/uploads/2020/03/500x500.png"
-            alt=""
-            className="w-[170px] h-[170px] object-cover bg-gray-500 rounded-lg"
-          />
-          <div>Сальник 212.232</div>
-        </div>
-        <div>
-          <img
-            src="https://utmunbox.com/wp-content/uploads/2020/03/500x500.png"
-            alt=""
-            className="w-[170px] h-[170px] object-cover bg-gray-500 rounded-lg"
+            className="object-cover bg-gray-500 rounded-lg"
           />
           <div>Сальник 212.232</div>
         </div>
