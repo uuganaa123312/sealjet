132621eefc29d4d3e157184566630225620a7f81
uuganbayar dashdavaa <uuganbayar@erdenetmc.mn>
2022-06-29 19:31:50 +0800

    Asd

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,23 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
-      <iframe
-        title="Sealjet"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1337.2072017296773!2d106.93330889252444!3d47.90901857385925!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5d9693fe35a7e1d9%3A0x9216d2241acc9e8c!2sSealjet%20Mongolia!5e0!3m2!1sen!2smn!4v1656399655470!5m2!1sen!2smn"
-        width="100%"
-        height={300}
-        className="pt-4"
-      />
+      <div className="hidden lg:block bg-[#395C4D] ">
+        <div className="flex items-center justify-between max-w-7xl mx-auto px-20">
+          <div className="h-[300px] flex flex-col items-center justify-center text-white">
+            <div>Email: Sealjetmongolia</div>
+            <div>7711 9596</div>
+          </div>
+          <div>
+            <iframe
+              title="Sealjet"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1337.2072017296773!2d106.93330889252444!3d47.90901857385925!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5d9693fe35a7e1d9%3A0x9216d2241acc9e8c!2sSealjet%20Mongolia!5e0!3m2!1sen!2smn!4v1656399655470!5m2!1sen!2smn"
+              width={500}
+              height={250}
+            />
+          </div>
+        </div>
+      </div>
+
       <div className="relative w-full text-center bg-white pt-6 pb-2 border-t">
         <div className="flex flex-row items-center justify-between mx-4">
           <div className="p-2 bg-[#395C4D] w-[100px] rounded-2xl flex items-center justify-center">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -280,24 +280,33 @@ const Home = () => {
   return (
     <div className="pt-[58px]">
       <div className="">
-        <div className="bg-black h-[300px] sm:h-auto">
+        <div className="bg-black">
           <img
             src="/img/home-bg.png"
             alt=""
-            className="opacity-40 object-cover h-[300px] sm:h-auto"
+            className="opacity-40 object-cover h-[300px] w-full sm:h-[400px] md:h-[500px] lg:h-[600px] xl:h-[700px]"
           />
         </div>
         <div className="absolute top-[179px] font-bold text-white text-xl w-full text-center uppercase">
-          Сальникийн төрөлжсөн дэлгүүр
-          <br />
-          <span className="text-xs opacity-60 font-normal hidden sm:block">
-            Монгол - Австрийн хамтарсан Сийл Жет Монгол ХХК нь 2003 оноос эхлэн
-            бүх төрлийн авто машин, техник тоног төхөөрөмжийн нягтруулагч
-            цагираг, жийргэвч ( сальник )-ийг үйлдвэрлэн дотоодын зах зээлд
-            нийлүүлж байна.
-          </span>
+          <div className="flex items-center justify-center max-w-7xl mx-auto px-4">
+            <img
+              src="/img/home-s.png"
+              alt=""
+              className="w-[40%] h-[280px] object-cover hidden md:block md:flex-1"
+            />
+            <div className="w-[60%] lg:text-3xl">
+              Сальникийн төрөлжсөн дэлгүүр
+              <br />
+              <span className="text-xs opacity-60 font-normal hidden sm:block md:flex-1 md:pt-2 lg:text-base">
+                Монгол - Австрийн хамтарсан Сийл Жет Монгол ХХК нь 2003 оноос
+                эхлэн бүх төрлийн авто машин, техник тоног төхөөрөмжийн
+                нягтруулагч цагираг, жийргэвч ( сальник )-ийг үйлдвэрлэн
+                дотоодын зах зээлд нийлүүлж байна.
+              </span>
+            </div>
+          </div>
         </div>
-        <div className="py-5 -mt-20">
+        <div className="py-5 -mt-20 lg:-mt-40 lg:py-10">
           <Carousel
             responsive={responsive}
             draggable={false}
@@ -310,41 +319,41 @@ const Home = () => {
             <img
               src="/img/Rectangle 348.png"
               alt=""
-              className="w-10 h-10 object-cover"
+              className="w-10 h-10 object-cover lg:w-20 lg:h-20"
             />
             <img
               src="/img/Rectangle 349.png"
               alt=""
-              className="w-10 h-10 object-cover"
+              className="w-10 h-10 object-cover lg:w-20 lg:h-20"
             />
             <img
               src="/img/Rectangle 350.png"
               alt=""
-              className="w-10 h-10 object-cover"
+              className="w-10 h-10 object-cover lg:w-20 lg:h-20"
             />
             <img
               src="/img/Rectangle 351.png"
               alt=""
-              className="w-10 h-10 object-cover"
+              className="w-10 h-10 object-cover lg:w-20 lg:h-20"
             />
             <img
               src="/img/Rectangle 352.png"
               alt=""
-              className="w-10 h-10 object-cover"
+              className="w-10 h-10 object-cover lg:w-20 lg:h-20"
             />
             <img
               src="/img/Rectangle 353.png"
               alt=""
-              className="w-10 h-10 object-cover"
+              className="w-10 h-10 object-cover lg:w-20 lg:h-20"
             />
             <img
               src="/img/Rectangle 354.png"
               alt=""
-              className="w-10 h-10 object-cover"
+              className="w-10 h-10 object-cover lg:w-20 lg:h-20"
             />
           </Carousel>
         </div>
-        <div className="px-4 py-4 max-w-7xl mx-auto">
+        <div className="px-4 py-4 max-w-7xl mx-auto lg:my-6">
           <div className="text-[#253D32] text-xl font-bold pb-2 flex items-center justify-between">
             <div>Бүтээгдэхүүн</div>
             <div
@@ -376,7 +385,7 @@ const Home = () => {
             })}
           </Carousel>
         </div>
-        <div className="px-4 pb-4 max-w-7xl mx-auto">
+        <div className="px-4 pb-4 max-w-7xl mx-auto lg:my-6">
           <div className="text-[#253D32] text-xl font-bold pb-2 flex items-center justify-between">
             <div>Материал</div>
             <div
@@ -408,7 +417,7 @@ const Home = () => {
             })}
           </Carousel>
         </div>
-        <div className="px-4 pb-4 max-w-7xl mx-auto">
+        <div className="px-4 pb-4 max-w-7xl mx-auto lg:my-6">
           <div className="text-[#253D32] text-xl font-bold pb-2 flex items-center justify-between">
             <div>Мэдээ, мэдээлэл</div>
             <div
