1ce7b1caa433cf4522cc251746556b969c09e955
uuganbayar dashdavaa <uuganbayar@erdenetmc.mn>
2022-07-21 13:46:30 +0800

    asd

--- src/api/requests.js
+++ src/api/requests.js
@@ -3,7 +3,7 @@ import Swal from "sweetalert2";
 
 const URL = "https://mmmall.mn/api/sealjet";
 
-export function MISHEEL() {
+export function SEALJET() {
   return createInstance(URL);
 }
 
@@ -57,195 +57,24 @@ const createInstance = (baseURL) => {
   return api;
 };
 
-// Нүүр хуудас
-
-export async function postImg(formData) {
-  const response = await MISHEEL().post("/admin", formData);
+// Footer
+export async function postFooter(formData) {
+  const response = await SEALJET().post("/footer", formData);
   return response;
 }
 
-export async function putImg(formData, id) {
-  const response = await MISHEEL().put("/admin/" + id, formData);
+export async function putFooter(formData, id) {
+  const response = await SEALJET().put("/footer/" + id, formData);
   return response;
 }
 
-export async function getImg() {
-  const response = await MISHEEL().get("/admin");
+export async function getFooter() {
+  const response = await SEALJET().get("/footer");
   return response;
 }
 
-export async function deleteImg(id) {
-  const response = await MISHEEL().delete("/admin/" + id);
+export async function deleteFooter(id) {
+  const response = await SEALJET().delete("/footer/" + id);
   return response;
 }
-
-export async function getBanner(id) {
-  const response = await MISHEEL().get("/banner/" + id);
-  return response;
-}
-export async function postBanner(formData) {
-  const response = await MISHEEL().post("/banner", formData);
-  return response;
-}
-// Нүүр хуудас
-// Брэнд
-
-export async function postBrand(formData) {
-  const response = await MISHEEL().post("/brand", formData);
-  return response;
-}
-export async function putBrand(formData, id) {
-  const response = await MISHEEL().put("/brand/" + id, formData);
-  return response;
-}
-export async function getBrand() {
-  const response = await MISHEEL().get("/brand");
-  return response;
-}
-export async function getOneBrand(id) {
-  const response = await MISHEEL().get("/brand/" + id);
-  return response;
-}
-export async function deleteBrand(id) {
-  const response = await MISHEEL().delete("/brand/" + id);
-  return response;
-}
-// Брэнд
-// Category
-export async function postCategory(params) {
-  const response = await MISHEEL().post("/category", {
-    ...params,
-  });
-  return response;
-}
-export async function getCategory() {
-  const response = await MISHEEL().get("/category");
-  return response;
-}
-export async function postSubCategory(params) {
-  const response = await MISHEEL().post("/subcategory", {
-    ...params,
-  });
-  return response;
-}
-export async function getSubCategory() {
-  const response = await MISHEEL().get("/subcategory");
-  return response;
-}
-// Category
-// Product
-export async function postProduct(formData) {
-  const response = await MISHEEL().post("/product", formData);
-  return response;
-}
-export async function getProduct() {
-  const response = await MISHEEL().get("/product");
-  return response;
-}
-export async function deleteProduct(id) {
-  const response = await MISHEEL().delete("/product/" + id);
-  return response;
-}
-// Product
-// Price
-export async function postPrice(formData) {
-  const response = await MISHEEL().post("/price", formData);
-  return response;
-}
-export async function getPrice() {
-  const response = await MISHEEL().get("/price");
-  return response;
-}
-export async function deletePrice(id) {
-  const response = await MISHEEL().delete("/price/" + id);
-  return response;
-}
-// Cover
-export async function postPriceCover(formData) {
-  const response = await MISHEEL().post("/priceHead", formData);
-  return response;
-}
-export async function getPriceCover() {
-  const response = await MISHEEL().get("/priceHead");
-  return response;
-}
-// Cover
-// Price
-// Social
-export async function postSocial(name, formData) {
-  const response = await MISHEEL().post("/" + name, formData);
-  return response;
-}
-export async function getSocial(name) {
-  const response = await MISHEEL().get("/" + name);
-  return response;
-}
-export async function deleteSocial(name, id) {
-  const response = await MISHEEL().delete("/" + name + "/" + id);
-  return response;
-}
-// Social
-// About
-export async function postAbout(formData) {
-  const response = await MISHEEL().post("/about", formData);
-  return response;
-}
-export async function putAbout(id, formData) {
-  const response = await MISHEEL().put("/about/" + id, formData);
-  return response;
-}
-export async function getAbout() {
-  const response = await MISHEEL().get("/about");
-  return response;
-}
-export async function deleteAboutRelated(id) {
-  const response = await MISHEEL().delete("/about/related" + id);
-  return response;
-}
-export async function getTimeline() {
-  const response = await MISHEEL().get("/timeline");
-  return response;
-}
-export async function postTimeline(formData) {
-  const response = await MISHEEL().post("/timeline", formData);
-  return response;
-}
-export async function deleteTimeline(id) {
-  const response = await MISHEEL().delete("/timeline/" + id);
-  return response;
-}
-export async function postUser(params) {
-  const response = await MISHEEL().post("/auth/user", {
-    ...params,
-  });
-  return response;
-}
-export async function putUser(id, params) {
-  const response = await MISHEEL().put("/auth/user/" + id, {
-    ...params,
-  });
-  return response;
-}
-export async function deleteUser(id) {
-  const response = await MISHEEL().delete("/auth/user/" + id);
-  return response;
-}
-export async function getUser() {
-  const response = await MISHEEL().get("/auth/user");
-  return response;
-}
-// About
-// Service
-export async function getService() {
-  const response = await MISHEEL().get("/service");
-  return response;
-}
-export async function postServie(formData) {
-  const response = await MISHEEL().post("/service", formData);
-  return response;
-}
-export async function deleteServie(id) {
-  const response = await MISHEEL().delete("/service/" + id);
-  return response;
-}
-// Service
+// Footer
--- src/components/Admin/Setting.js
+++ src/components/Admin/Setting.js
@@ -2,6 +2,14 @@ import React, { useState } from "react";
 
 const Setting = () => {
   const [footer, setFooter] = useState(false);
+  const [address, setAddress] = useState("");
+  const [url, setUrl] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [facebook, setFacebook] = useState("");
+  const [instagram, setInstagram] = useState("");
+  const [twitter, setTwitter] = useState("");
+
   return (
     <div className="font-[roboto] mx-4">
       <div
@@ -13,35 +21,87 @@ const Setting = () => {
       {/* footer */}
       {footer && (
         <div className="">
+          <div className="flex justify-center items-center w-full mt-2">
+            <label
+              htmlFor="dropzone-file"
+              className="flex flex-col justify-center items-center w-full h-64 bg-gray-100 rounded-lg border-2 border-gray-100 border-dashed cursor-pointer dark:hover:bg-bray-100 dark:bg-gray-100 hover:bg-gray-100 dark:border-gray-400 dark:hover:border-gray-500 dark:hover:bg-gray-100"
+            >
+              <div className="flex flex-col justify-center items-center pt-5 pb-6">
+                <svg
+                  aria-hidden="true"
+                  className="mb-3 w-10 h-10 text-gray-400"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                  />
+                </svg>
+                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                  <span className="font-semibold">Click to upload</span> or drag
+                  and drop
+                </p>
+                <p className="text-xs text-gray-500 dark:text-gray-400">
+                  PNG or JPG (MAX. 400x230px)
+                </p>
+              </div>
+              <input
+                id="dropzone-file"
+                type="file"
+                className="hidden"
+                accept="image/png, image/jpeg"
+                value={url}
+                onChange={(e) => setUrl(e.target.files[0])}
+              />
+            </label>
+          </div>
+
           <input
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-2"
             type="text"
             placeholder="Хаяг"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
           />
           <input
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-2"
             type="text"
             placeholder="Утас"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
           />
           <input
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-2"
             type="text"
             placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <input
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-2"
             type="text"
             placeholder="Facebook"
+            value={facebook}
+            onChange={(e) => setFacebook(e.target.value)}
           />
           <input
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-2"
             type="text"
             placeholder="Instagram"
+            value={instagram}
+            onChange={(e) => setInstagram(e.target.value)}
           />
           <input
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-2"
             type="text"
             placeholder="Twitter"
+            value={twitter}
+            onChange={(e) => setTwitter(e.target.value)}
           />
         </div>
       )}
